7eee72deda59ae3fe572679c8955c6993a96c572
Waleed-Ahmed-05 <LP.Gangster05@gmail.com>
2024-11-28 19:43:23 +0500

    Added Medicine Database

---
+++ DAMS/Models/Medicine(Model)/Medicine.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DAMS.Models.Medicine_Model_
+{
+    public class Medicine
+    {
+        [Key]
+        public int Medicine_ID { get; set; }
+        [Required]
+        public string Medicine_Name { get; set; }
+        [Required]
+        public string Medicine_Description { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public int Weightage_ID { get; set; }
+        [Required]
+        public int Price_ID { get; set; }
+    }
+}
---
+++ DAMS/Models/Medicine(Model)/Weightage.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.Identity.Client;
+
+namespace DAMS.Models.Medicine_Model_
+{
+    public class Weightage
+    {
+        [Key]
+        public int Weightage_ID { get; set; }
+        [Required]
+        public string Medicine_Weightage {  get; set; }
+    }
+}
